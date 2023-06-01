6bc7dcc1a5d69d1b06d2af895a486986ab4f230e
Tim <Timratsko909@gmail.com>
2023-05-31 17:17:37 -0700

    Cleaned up code in the map class

--- src/view/gui/Map.java
+++ src/view/gui/Map.java
@@ -5,7 +5,6 @@ import model.dungeon.tile.Cell;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
@@ -34,53 +33,17 @@ public class Map extends JPanel {
         createTheTileMap(theMaze);
 
         AddCharacter(0, 0);
-
-//        String sampleMonsterPath = "res" + File.separator + "SampleMonster.png";
-//
-//        for (int i = 1; i < 10; i++) {
-//            addEntity(i, 5, sampleMonsterPath);
-//        }
     }
 
     private void createTheTileMap(Cell[][] theMaze) {
-
-
-
-
-        String tilePath = "res" + File.separator + "DarkTile.png";
-        File tileFile = new File(tilePath);
-        String leftWallPath = "res" + File.separator + "DarkTileLeftWall.png";
-        File leftWallFile = new File(leftWallPath);
-
-        BufferedImage greyTileImage; BufferedImage leftWallImage;
-        try {
-            greyTileImage = ImageIO.read(tileFile);
-            leftWallImage =  ImageIO.read(leftWallFile);
-        } catch (Exception e) {
-            System.out.println("Had an issue loading the tile " + e);
-            return;
-        }
-
-
         for (int i = 0; i < myHeight; i++) {
             for (int j = 0; j < myWidth; j++) {
                 myPanels[i][j] = new JPanel();
                 myPanels[i][j].setLayout(new OverlayLayout(myPanels[i][j]));
 
-
-
-//                if (j == 0) {
-//                    myPanels[i][j].add(BorderLayout.CENTER,new JLabel(new ImageIcon(leftWallImage)));
-//                } else {
-//                    myPanels[i][j].add(BorderLayout.CENTER,new JLabel(new ImageIcon(greyTileImage)));
-//                }
-
-
-                myPanels[i][j].setPreferredSize(new Dimension(64,64));
+                myPanels[i][j].setPreferredSize(new Dimension(PIXELS_IN_SPRITE,PIXELS_IN_SPRITE));
 
                 this.add(myPanels[i][j]);
-
-
             }
         }
 
@@ -97,33 +60,8 @@ public class Map extends JPanel {
             JLabel player = new JLabel(new ImageIcon(ImageIO.read(new File(playerPath))));
             myPanels[theY][theX].add(player,0);
         } catch (Exception e) {
-            System.out.println("Something Bad just happened" + e);
+            System.out.println("Something Bad just happened when trying to load the character art " + e);
         }
-    }
-
-    private void addDoor(int theX, int theY) {
-        String doorPath = "res" + File.separator + "Door.png";
-        JLabel door = null;
-        try {
-            door = new JLabel(new ImageIcon(ImageIO.read(new File(doorPath))));
-        } catch (IOException e) {
-            System.out.println("There was an issue loading the door " + e);
-            return;
-        }
-        myPanels[theY][theX].removeAll();
-        myPanels[theY][theX].add(door,0);
-    }
-
-    private void addMonster(int theX, int theY) {
-        String sampleMonsterPath = "res" + File.separator + "SampleMonster.png";
-        JLabel monster = null;
-        try {
-            monster = new JLabel(new ImageIcon(ImageIO.read(new File(sampleMonsterPath))));
-        } catch (IOException e) {
-            System.out.println("There was an issue loading the Monster " + e);
-            return;
-        }
-        myPanels[theY][theX].add(monster,0);
     }
 
     private void addEntity(int theX, int theY, String thePath) {
@@ -145,9 +83,9 @@ public class Map extends JPanel {
             System.out.println("There was an issue loading the Entity with path " + thePath + "\n with error "  + e);
             return;
         }
-        if (myPanels[theY][theX].getComponents().length != 0){
-            myPanels[theY][theX].removeAll();
-        }
+        // Remove all the previous art since if the tile is being changed then nothing else should be visible,
+        // nothing else could possibly be on top of a tile
+        myPanels[theY][theX].removeAll();
         myPanels[theY][theX].add(tileLabel,0);
     }
 }
