e11f178d007f2a4866e0cb75225f1c56ccea2e49
Tim <Timratsko909@gmail.com>
2023-06-06 18:22:14 -0700

    Created the WallTest class but did not implement any tests as they are not necessary

---
+++ src/test/WallTest.java
@@ -0,0 +1,24 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WallTest {
+
+    @Test
+    void getX() {
+        return;
+    }
+
+    @Test
+    void getY() {
+        return;
+    }
+
+    @Test
+    void testToString() {
+        return; // TODO figure out if this needs testing
+        // This returns two lines of random characters to display a wall so it shouldn't need testing
+    }
+}
