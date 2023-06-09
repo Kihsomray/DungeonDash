6b0c3ab183edfb598d171c6a8c6b2ff060459a68
Tim <Timratsko909@gmail.com>
2023-06-09 14:17:36 -0700

    Finished the DungeonTest class

---
+++ src/test/finished/DungeonTest.java
@@ -0,0 +1,38 @@
+package test.finished;
+
+import model.dungeon.Dungeon;
+import model.entity.hero.Hero;
+import model.entity.hero.Warrior;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/** Class to test the Door object. */
+class DungeonTest {
+
+    /** Dungeon to test this class with. */
+    private Dungeon myDungeon;
+    /** Hero to test if it is being stored correctly. */
+    private Hero myHero;
+
+    /** Create the dungeon to test with. */
+    @BeforeEach
+    void setUp() {
+        myHero = new Warrior("dummy");
+        myDungeon = new Dungeon(null, 5, 5, myHero);
+    }
+
+    /** Test if hero is set correctly. */
+    @Test
+    void getHero() {
+        assertEquals(myHero, myDungeon.getHero());
+    }
+
+    /** Test if the isGamePlaying returns true. */
+    @Test
+    void isGamePlaying() {
+        // Player shouldn't have entered the exit yet. so it should still be 'playing'
+        assertTrue(myDungeon.isGamePlaying());
+    }
+}
