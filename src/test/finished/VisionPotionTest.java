23ccdddb7c7d735288784ac51f5b49a374bbb8c8
Tim <Timratsko909@gmail.com>
2023-06-09 16:26:01 -0700

    Javadoc VisionPotionTest

--- src/test/finished/VisionPotionTest.java
+++ src/test/finished/VisionPotionTest.java
@@ -8,15 +8,19 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/** Class to test VisionPotion. */
 class VisionPotionTest {
 
+    /** VisionPotion to test with. */
     VisionPotion myVisionPotion;
 
+    /** Set up the VisionPotion. */
     @BeforeEach
     void setUp() {
         myVisionPotion = new VisionPotion();
     }
 
+    /** Testing applying the VisionPotion to a dummy hero. */
     @Test
     void applyPotion() {
         Hero testHero = new Warrior("dummy");
@@ -25,6 +29,7 @@ class VisionPotionTest {
         assertTrue(testHero.isExtraVisibility());
     }
 
+    /** Test the display character. */
     @Test
     void getDisplayChar() {
         assertEquals('V', myVisionPotion.getDisplayChar());
