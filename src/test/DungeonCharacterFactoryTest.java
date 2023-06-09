25ff36cce07968b90e06f2d5eedef800449fcee4
Tim <Timratsko909@gmail.com>
2023-06-09 15:48:32 -0700

    Finished the DungeonCharacterFactoryTest

--- src/test/DungeonCharacterFactoryTest.java
+++ src/test/DungeonCharacterFactoryTest.java
@@ -1,5 +1,7 @@
 package test;
 
+import model.entity.DungeonCharacterFactory;
+import model.entity.enemy.monster.Monster;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,5 +10,7 @@ class DungeonCharacterFactoryTest {
 
     @Test
     void generateMonster() {
+        Monster m = DungeonCharacterFactory.generateMonster();
+        assertEquals('M', m.getDisplayChar());
     }
 }
