a395a172ca8990a14e624f7537784bf06b32fe51
Tim <Timratsko909@gmail.com>
2023-06-09 14:36:07 -0700

    Tested every method in Battle, in BattleTest

---
+++ src/test/finished/BattleTest.java
@@ -0,0 +1,56 @@
+package test.finished;
+
+import model.entity.battle.Battle;
+import model.entity.enemy.monster.Monster;
+import model.entity.enemy.monster.mock.MockSkeleton;
+import model.entity.hero.Hero;
+import model.entity.hero.Warrior;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/** Class to test Battle. */
+class BattleTest {
+
+    /** Hero to test Battle. */
+    Hero myHero;
+    /** Monster to test Battle. */
+    Monster myMonster;
+    /** Battle to test the Battle class. */
+    Battle myBattle;
+
+    /** Set up the hero, monster and battle to test with. */
+    @BeforeEach
+    void setUp() {
+        myHero = new Warrior("test");
+        myMonster = new MockSkeleton();
+        myBattle = new Battle(myHero, myMonster);
+    }
+
+    /** Test if hero attacking the monster results with what is expected. */
+    @Test
+    void heroAttackMonster() {
+        Battle.Result result = myBattle.heroAttackMonster(Battle.Option.ATTACK);
+        assertEquals(Battle.Result.NORMAL_HERO, result);
+    }
+
+    /** Test if monster attacking the hero results with what is expected. */
+    @Test
+    void monsterAttackHero() {
+        Battle.Result result = myBattle.monsterAttackHero();
+        assertEquals(Battle.Result.NORMAL_MONSTER, result);
+    }
+
+    /** Test if you can get the monster after stuff in the battle happens. */
+    @Test
+    void getMonster() {
+        assertEquals(myMonster, myBattle.getMonster());
+    }
+
+    /** Test if hero has the ability, which is true since it was never used in testing. */
+    @Test
+    void hasAbility() {
+        assertTrue(myBattle.hasAbility());
+    }
+}
