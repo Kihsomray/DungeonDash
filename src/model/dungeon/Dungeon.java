9a4e4c216da1cde0a40a4c9580f22bc206fed2d9
Kihsomray <zerotoil.network@gmail.com>
2023-06-09 01:13:32 -0700

    Added Dungeon/Generator JavaDoc

--- src/model/dungeon/Dungeon.java
+++ src/model/dungeon/Dungeon.java
@@ -10,24 +10,31 @@ import model.entity.hero.Hero;
 import java.io.Serializable;
 
 /**
+ * A dungeon contains the back-end data for the DungeonAdventure game. This
+ * data ranges from a 2D array of mazes, the hero and all of its data, along
+ * with any other necessary data.
  *
+ * @version 1.0.0
  * @author Kihsomray
  * @author Patrick Hern
  */
 public class Dungeon implements Serializable {
 
+    /** Main instance of the program. */
     private final DungeonAdventure myMain;
 
     /** The dungeon/maze made of Rooms. */
     private final Cell[][] myMaze;
 
+    /** Exit door of the dungeon. */
     private final Door myExit;
 
+    /** Hero corresponding to this dungeon. */
     private final Hero myHero;
 
+    /** Number of rooms in the dungeon. */
     private final int myRoomCount;
-    private final int myWidth;
-    private final int myHeight;
+
 
     /**
      * Constructor for the dungeon that creates a new dungeon
@@ -42,8 +49,6 @@ public class Dungeon implements Serializable {
 
         myMain = theMain;
         myHero = theHero;
-        myWidth = theWidth;
-        myHeight = theHeight;
 
         // Initialize the Dungeon using Prim's generator.
         final DungeonGenerator generator = new PrimsGenerator(theWidth, theHeight, theHero);
@@ -51,6 +56,7 @@ public class Dungeon implements Serializable {
         // Generate the maze.
         myMaze = generator.generate();
 
+        // Gets the exit.
         myExit = generator.getExit();
 
         // Gets the room count.
@@ -58,26 +64,57 @@ public class Dungeon implements Serializable {
 
     }
 
+
+    /**
+     * Gets the hero of this dungeon.
+     *
+     * @return Hero binded to this dungeon.
+     */
     public Hero getHero() {
         return myHero;
     }
 
+    /**
+     * Gets the maze of the dungeon.
+     *
+     * @return Maze contained in the dungeon.
+     */
     public Cell[][] getMaze() {
         return myMaze;
     }
 
+    /**
+     * Gets the number of rooms contained in the dungeon.
+     *
+     * @return Rooms in the dungeon.
+     */
     public int getRoomCount() {
         return myRoomCount;
     }
 
+    /**
+     * Gets the width of the dungeon.
+     *
+     * @return Width of the dungeon.
+     */
     public int getMyWidth() {
-        return myWidth;
+        return myMaze.length;
     }
 
+    /**
+     * Gets the height of the dungeon.
+     *
+     * @return Height of the dungeon.
+     */
     public int getMyHeight() {
-        return myHeight;
+        return myMaze[0].length;
     }
 
+    /**
+     * Checks if player has walked through the exit.
+     *
+     * @return Is the game still being played.
+     */
     public boolean isGamePlaying() {
         return !myExit.hasEntered();
     }
