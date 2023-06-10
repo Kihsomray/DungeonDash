b2c06b7de10ec4fa85a4b065065505bd688ca8cc
Tim <Timratsko909@gmail.com>
2023-06-09 19:09:00 -0700

    Almost finished RoomTest, I cant test toString

--- src/test/RoomTest.java
+++ src/test/RoomTest.java
@@ -2,7 +2,14 @@ package test;
 
 import model.Interactable;
 import model.dungeon.cell.passable.Room;
+import model.entity.enemy.Trap;
+import model.entity.enemy.monster.Monster;
+import model.entity.hero.Hero;
+import model.entity.hero.Warrior;
+import model.inventory.Inventory;
+import model.inventory.item.Item;
 import model.inventory.item.potion.HealthPotion;
+import model.inventory.item.potion.VisionPotion;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -10,25 +17,51 @@ import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/** Testing the Room class. */
 class RoomTest {
 
     Room myRoom;
 
+    /** Setting up a Room. */
     @BeforeEach
     void setUp() {
-        myRoom = new Room(1, 1);
+        myRoom = new Room(1, 2);
     }
 
     @Test
-    void getNeighbors() {
-        assertEquals("## ## ##\n" +
-                "##    ##\n" +
-                "## ## ##\n", myRoom.getNeighbors().toString());
+    void testToString() {
+        String outStr = "     \n" +
+                "     ";
+        Set<Interactable> set = myRoom.getInteractables();
+
+        for (Interactable i : set) {
+            if (i instanceof Monster) {
+                outStr = "\u001B[31mM" + outStr;
+            }
+            if (i instanceof HealthPotion) {
+                outStr = "\u001B[36mH " + outStr;
+            } if (i instanceof VisionPotion) {
+                outStr = "\u001B[36mV" + outStr;
+            } if (i instanceof Trap) {
+                outStr = "\u001B[32mT  " + outStr;
+            }
+        }
+
+        outStr.replace(" ", ""); // TODO FIGURE THIS OUT
+
+        assertEquals(outStr, myRoom.toString().replace(" ", ""));
     }
 
     @Test
     void interactWith() {
-        return; // Method does nothing
+        Hero dHero = new Warrior("test");
+
+        Inventory roomInv = myRoom.getInventory();
+        myRoom.interactWith(dHero);
+
+        for (Item i : roomInv.getInventory()) {
+            assertTrue(dHero.getInventory().getInventory().contains(i));
+        }
     }
 
     @Test
@@ -38,41 +71,6 @@ class RoomTest {
 
     @Test
     void getY() {
-        assertEquals(1, myRoom.getX());
-    }
-
-    @Test
-    void getInventory() {
-        return; // Simple getter
-    }
-
-    @Test
-    void getEntities() {
-        return; // Simple getter
-    }
-
-    @Test
-    void getEnemies() {
-        return; // Simple getter
-    }
-
-    @Test
-    void testToString() {
-        String expected = "";
-
-
-//        for (Interactable i : roomInv) {
-////            if (i.getDisplayChar() == 'H') {
-////                expected = expected + "\u001B[36mH";
-////            } if (i.getDisplayChar() == 'M') {
-////                expected = expected + "\u001B[31mM";
-////            }
-////        }
-        expected = expected + "    \n     ";
-
-        //System.out.println(myRoom.getEntities());
-
-
-        assertEquals(expected, myRoom.toString());
+        assertEquals(2, myRoom.getY());
     }
 }
